ff108b5f291d64072022d6b34ae03fde2618e809
TariqueNasrullah <nasrullahtarique@gmail.com>
2021-11-03 02:32:09 +0600

    make use of the book & author repository

--- infra/mocks/ArangoDB.go
+++ infra/mocks/ArangoDB.go
@@ -62,6 +62,29 @@ func (_m *ArangoDB) DocumentExists(ctx context.Context, col string, key string) 
 	return r0, r1
 }
 
+// Query provides a mock function with given fields: ctx, query, binVars
+func (_m *ArangoDB) Query(ctx context.Context, query string, binVars map[string]interface{}) (interface{}, error) {
+	ret := _m.Called(ctx, query, binVars)
+
+	var r0 interface{}
+	if rf, ok := ret.Get(0).(func(context.Context, string, map[string]interface{}) interface{}); ok {
+		r0 = rf(ctx, query, binVars)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(interface{})
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, string, map[string]interface{}) error); ok {
+		r1 = rf(ctx, query, binVars)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // ReadDocument provides a mock function with given fields: ctx, col, key, result
 func (_m *ArangoDB) ReadDocument(ctx context.Context, col string, key string, result interface{}) error {
 	ret := _m.Called(ctx, col, key, result)
